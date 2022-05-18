a4975f88d31a469e69916ffa67fd78e6f378aebc
Doris Morales <moralesdoris880@gmail.com>
2022-05-18 16:51:14 -0400

    Routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  resources :pages
+  post '/signup', to: 'users#create'
+  get '/me', to: 'users#show'
 
+  get '*path',
+      to: 'fallback#index',
+      constraints: ->(req) { !req.xhr? && req.format.html? }
 end

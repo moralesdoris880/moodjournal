bfaa2a5022e7000dad901be594c0bc05ee7e02fa
Doris Morales <moralesdoris880@gmail.com>
2022-05-18 17:14:51 -0400

    App page wip

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        {/* <Route path="/login" element={}/> NEW ENTRY */}
+        {/* <Route path="/login" element={}/> LIST OF PAGES */}
+        {/* <Route path="/signup" element={}/> SIGNUP PAGE */}
+        {/* <Route path="/login" element={}/> LOGIN PAGE */}
+        {/* <Route path="/" element={<Home />}/> HOME PAGE */}
+      </Routes>
     </div>
+    </BrowserRouter>
   );
 }
 
